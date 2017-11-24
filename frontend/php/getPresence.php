7c37962389b05c83d25af27b011d0f3db9344583
Ryan Jacobs <rjacobs@422459.no-reply.drupal.org>
2017-11-24 11:00:12 -0600

    Fix labels reversed on Presence History graph on energy page.

--- frontend/php/getPresence.php
+++ frontend/php/getPresence.php
@@ -7,7 +7,7 @@ $connection = mysqli_connect($ini['mysql']['mysql_hostname'],$ini['mysql']['mysq
 $sql = "SELECT date, ROUND(SUM(auto_away)) AS auto_away FROM status WHERE (DATE(date) BETWEEN (date('".$_GET['start']."')) AND (date('".$_GET['end']."'))) 
 		GROUP BY DAY(date) ORDER BY date ASC;";
 $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
-$sql_array = array("cols" => array(array("label"=>"Date", "type"=>"datetime"),array("label"=>"At Home","type"=>"number"), array("label"=>"Auto-Away","type"=>"number")));
+$sql_array = array("cols" => array(array("label"=>"Date", "type"=>"datetime"),array("label"=>"Auto-Away","type"=>"number"), array("label"=>"At Home","type"=>"number")));
 while($row =mysqli_fetch_assoc($result))
 {
     $phpdate = strtotime($row['date']);
